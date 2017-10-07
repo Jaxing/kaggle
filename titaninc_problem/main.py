e4e44b07728d0444f65c8708ba0f5f7b8f64e139
Jesper Jaxing <jaxing@student.chalmers.se>
2017-10-07 15:27:04 +0200

    cleans up code

--- titaninc_problem/main.py
+++ titaninc_problem/main.py
@@ -1,17 +1,11 @@
-import keras
 from keras.models import Sequential
-from keras.layers import Dense, Activation
+from keras.layers import Dense
 import numpy as np
 import pandas
 import csv
 
-# Most likely not useful
-gender_submission_data = pandas.read_csv('./gender_submission.csv')
-
-
 training_data = pandas.read_csv('./train.csv')
 test_data = pandas.read_csv('./test.csv')
-# print(training_data)
 
 training_data_input = (training_data.loc[:, ['Pclass', 'Sex']])\
                             .replace('female', 1)\
@@ -37,12 +31,7 @@ model.fit(
     epochs=10,
     batch_size=10
 )
-# score = model.evaluate(
-#                 np.array(training_data_input),
-#                 np.array(training_data_target),
-#                 batch_size=10
-#         )
-# print(score)
+
 online_prediction_model = Sequential()
 online_prediction_model.add(Dense(1, input_shape=(2,), activation='sigmoid'))
 online_prediction_model.compile(optimizer='rmsprop',
@@ -61,18 +50,6 @@ for i in range(0, len(test_data_input)):
     testX = testX.values.reshape(1, 2)
     yhat = online_prediction_model.predict(np.array(testX), batch_size=1)
     result.append([passenger_id, (1 if 0.5 < yhat[0][0] else 0)])
-# trained_weights = model.get_weights()
-#
-# singel_batch_model = Sequential([
-#     Dense(1, input_shape=(2, 1), activation='relu', use_bias=True)
-# ]).set_weights(trained_weights)
-#
-#
-#
-# for i in range(0, len(test_data_input)):
-#     hej = test_data_input[1]
-#     print(hej)
-#     result.append(["", test_data.loc[i, "PassengerId"], singel_batch_model.predict(hej, batch_size=1)])
 #
 with open('submission.csv', 'w') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
@@ -80,5 +57,3 @@ with open('submission.csv', 'w') as csvfile:
         print(row)
         row = map(str, row)
         writer.writerow(row)
-#
-# #print(score)
